0ba9cbe4e3e360193660c64e3945bd2b48e38887
scoefield <scoefield@163.com>
2019-05-26 17:15:04 +0800

    go with mongodb and test

---
+++ go-databases/go-mongo/go_mongodb.go
@@ -0,0 +1,129 @@
+package go_mongo
+
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
+// ××××××××××× 定义常量 ×××××××××××××××××
+const URL = "127.0.0.1:27017"
+
+var (
+	mgoSession *mgo.Session
+)
+
+// ×××××××××**×× 初始化init ×××××××××××××××××
+func init() {
+	session, err := mgo.Dial(URL)
+	if err != nil {
+		log.Fatalf("Create Session: %s\n", err)
+	}
+	mgoSession = session
+}
+
+// ××××××××××××××××**×× 获取连接 ×××××××××××××××××××××××
+func connect(db, collection string) (*mgo.Session, *mgo.Collection) {
+	ms := mgoSession.Copy()
+	c := ms.DB(db).C(collection)
+	ms.SetMode(mgo.Monotonic, true)
+	return ms, c
+}
+
+// ××××××××××××××××××××××**×× 插入 ××××××××××××××××××××××××××
+func Insert(db, collection string, doc interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Insert(doc)
+}
+
+// ××××××××××××××××××××××××××**×× 查询 ××××××××××××××××××××××××××
+/*
+db:操作的数据库
+collection:操作的文档(表)
+query:查询条件
+selector:需要过滤的数据(projection)
+result:查询到的结果
+*/
+func FindOne(db, collection string, query, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Find(query).Select(selector).One(result)
+}
+func FindAll(db, collection string, query, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Find(query).Select(selector).All(result)
+}
+
+// ××××××××××××××××××××××××××**×× 更新 ××××××××××××××××××××××××××
+/**
+db:操作的数据库
+collection:操作的文档(表)
+selector:更新条件
+update:更新的操作
+*/
+func Update(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Update(selector, update)
+}
+
+//更新，如果不存在就插入一个新的数据 `upsert:true`
+func Upsert(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.Upsert(selector, update)
+	return err
+}
+
+// `multi:true`
+func UpdateAll(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.UpdateAll(selector, update)
+	return err
+}
+
+// ××××××××××××××××××××××××××**×× 删除 ××××××××××××××××××××××××××
+/**
+db:操作的数据库
+collection:操作的文档(表)
+selector:删除条件
+*/
+func Remove(db, collection string, selector interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Remove(selector)
+}
+
+func RemoveAll(db, collection string, selector interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.RemoveAll(selector)
+	return err
+}
+
+// ××××××××××××××××××××××××××**×× 分页查询 ××××××××××××××××××××××××××
+/*
+db:操作的数据库
+collection:操作的文档(表)
+page:当前页面
+limit:每页的数量值
+query:查询条件
+selector:需要过滤的数据(projection)
+result:查询到的结果
+*/
+func FindPage(db, collection string, page, limit int, query, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	return c.Find(query).Select(selector).Skip(page * limit).Limit(limit).All(result)
+}
---
+++ go-databases/go-mongo/mongo_test.go
@@ -0,0 +1,73 @@
+package go_mongo
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"testing"
+)
+
+var (
+	database   = "Test"
+	collection = "TestModel"
+)
+
+//test data
+//type Data struct {
+//	//Id bson.ObjectId
+//	Title string
+//	Des string
+//	Content string
+//	Img string
+//	Date time.Time
+//}
+// test
+//func TestInsert(t *testing.T)  {
+//	data := &Data{
+//		//Id:      bson.NewObjectId().Hex(),
+//		Title:   "标题2",
+//		Des:     "博客描述信息2",
+//		Content: "博客的内容信息2",
+//		Img:     "https://upload-images.jianshu.io/upload_images/8679037-67456031925afca6.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/700",
+//		Date:    time.Now(),
+//	}
+//
+//	err := Insert("Test", "TestModel", data)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Println("insert success.")
+//}
+
+//func TestFindOne(t *testing.T) {
+//	var result Data
+//	err := FindOne("Test", "TestModel", bson.M{"title": "标题2"}, bson.M{"_id":0}, &result)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Println("find one success:", result)
+//}
+
+//func TestFindAll(t *testing.T) {
+//	var result []Data
+//	err := FindAll("Test", "TestModel", bson.M{"title": "标题2"}, bson.M{"_id":0}, &result)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Println("find all success:", result)
+//}
+
+//func TestUpdate(t *testing.T) {
+//	err := Update("Test", "TestModel", bson.M{"_id": "5cea46ca7eb409458018fe17"}, bson.M{"$set": bson.M{"title": "更新标题"}})
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Println("update success:")
+//}
+
+func TestRemove(t *testing.T) {
+	err := Remove(database, collection, bson.M{"_id": "5cea50d67eb409458018fedf"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("remove success")
+}
