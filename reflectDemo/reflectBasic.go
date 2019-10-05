3f7dc4bef1243ea24dbb8bc9cad0fb4d34b7300c
scoefield <scoefield@163.com>
2019-10-05 09:58:31 +0800

    [Add] reflect basic and struct practice

--- reflectDemo/reflectBasic.go
+++ reflectDemo/reflectBasic.go
@@ -27,7 +27,7 @@ func reflectTest01(b interface{}) {
 
 	// 3.下面将rValue转回interface{}
 	iV := rValue.Interface()
-	// 将 interface{} 通过断言转成需要的类型
+	// 将 interface{} 通过断言转成需要的类型值
 	num2 := iV.(int)
 	fmt.Println("num2=", num2)
 }
@@ -42,6 +42,14 @@ func reflectTest02(b interface{}) {
 	rValue := reflect.ValueOf(b)
 	fmt.Printf("rValue=%v, rValue type:%T\n", rValue, rValue)
 
+	// 获取变量的kind
+	// (1)rValue.kind()
+	// (2)rType.kind()
+	rValueKind := rValue.Kind()
+	fmt.Println("rValue.kind=", rValueKind)
+	rTypeKind := rType.Kind()
+	fmt.Println("rType.kind=", rTypeKind)
+
 	// 3.下面将rValue转回interface{}
 	iV := rValue.Interface()
 	fmt.Printf("iV=%v, iV type=%T\n", iV, iV)
@@ -53,10 +61,28 @@ func reflectTest02(b interface{}) {
 	}
 }
 
+// 通过反射修改num int的值
+// 修改结构体student的值
+func reflectTest03(b interface{}) {
+	// 获取到reflect.Value
+	rValue := reflect.ValueOf(b)
+	// 看看reflect的kind
+	rKind := rValue.Kind()
+	fmt.Println("rValue.kind=", rKind)
+	// 修改int num2的值，重要
+	rValue.Elem().SetInt(24)
+
+}
+
 func main() {
 	num := 100
 	reflectTest01(num)
 
 	stu := Student{"Jack", 23, "NewYork"}
 	reflectTest02(stu)
+
+	num2 := 23
+	// 通过反射修改值时记得传入该变量的地址
+	reflectTest03(&num2)
+	fmt.Println("change num2 value as:", num2)
 }
